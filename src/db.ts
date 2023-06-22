27ae05b5a9c654f304026518d3da996a2a11a49d
Nacho731 <elnachowe3@gmail.com>
2023-06-22 15:31:59 -0300

    rutas fix9

--- src/db.ts
+++ src/db.ts
@@ -4,23 +4,23 @@ config;
 
  ////////////////// *LOCAL* /////////////////////////
 
-export const sequelize = new Sequelize({
-  dialect: "postgres",
-  database: config.dbName,
-  password: config.dbPassword,
-  username: config.dbUser,
-  storage: ":memory:",
-  models: [__dirname + "/models"],
-});
+// export const sequelize = new Sequelize({
+//   dialect: "postgres",
+//   database: config.dbName,
+//   password: config.dbPassword,
+//   username: config.dbUser,
+//   storage: ":memory:",
+//   models: [__dirname + "/models"],
+// });
 
 //////////////// *DEPLOY* ///////////////////////
 
-// export const sequelize = new Sequelize({
-//   dialect: "postgres",
-//   database: "railway",
-//   password: "C65Qhq92tHC7iDdKnibe",
-//   username: "postgres",
-//   host: "containers-us-west-159.railway.app",
-//   port: 7322,
-//   models: [__dirname + "/models"],
-// });
+export const sequelize = new Sequelize({
+  dialect: "postgres",
+  database: "walletwise",
+  password: "XWq500msNDqt5hFrIijnlcfQ4Pt29zK3",
+  username: "walletwise_user",
+  host: "dpg-cia95b5gkuvu1mfjd4lg-a",
+  port: 5432,
+  models: [__dirname + "/models"],
+});
