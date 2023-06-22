05e01fb5466abb74bbc3847f4ef678a8e5046fd0
Nacho731 <elnachowe3@gmail.com>
2023-06-22 15:18:30 -0300

    rutas fix8

--- index.ts
+++ index.ts
@@ -1,32 +1,32 @@
 
 
-// import { sequelize } from './src/db';
-// import app from './src/app';
-// sequelize
-//   .sync({ force: true, logging: false })
-//   .then(() => {
-//     app.listen(3001, function () {
-//       console.log('Wake up, Neo...');
-//     });
-//   })
-//   .catch((err) => console.error(err));
-
-
-import 'reflect-metadata';
 import { sequelize } from './src/db';
 import app from './src/app';
+sequelize
+  .sync({ force: true, logging: false })
+  .then(() => {
+    app.listen(3001, function () {
+      console.log('Wake up, Neo...');
+    });
+  })
+  .catch((err) => console.error(err));
 
-const startServer = async () => {
-  try {
-    await sequelize.sync({ force: true, logging: false });
-    console.log('Base de datos sincronizada correctamente.');
 
-    return app;
-  } catch (error) {
-    console.error('Error al sincronizar la base de datos:', error);
-    throw error;
-  }
-};
+// import 'reflect-metadata';
+// import { sequelize } from './src/db';
+// import app from './src/app';
 
-export default startServer;
+// const startServer = async () => {
+//   try {
+//     await sequelize.sync({ force: true, logging: false })
+//     .then(() => {
+//           app.listen(3001, function () {
+//             console.log('Wake up, Neo...');
+//           });
+//           }
+//     .catch(())
+//     }
+// }
+
+// export default startServer;
 
