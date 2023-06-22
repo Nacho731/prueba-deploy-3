66862d1860bcadf172aa1f0c36d8639fadaedde2
Nacho731 <elnachowe3@gmail.com>
2023-06-22 14:23:50 -0300

    rutas fix5

---
+++ index.ts
@@ -0,0 +1,30 @@
+
+
+// import { sequelize } from './src/db';
+// import app from './src/app';
+// sequelize
+//   .sync({ force: true, logging: false })
+//   .then(() => {
+//     app.listen(3001, function () {
+//       console.log('Wake up, Neo...');
+//     });
+//   })
+//   .catch((err) => console.error(err));
+
+import { sequelize } from './src/db';
+import app from './src/app';
+
+const startServer = async () => {
+  try {
+    await sequelize.sync({ force: true, logging: false });
+    console.log('Base de datos sincronizada correctamente.');
+
+    return app;
+  } catch (error) {
+    console.error('Error al sincronizar la base de datos:', error);
+    throw error;
+  }
+};
+
+export default startServer;
+
